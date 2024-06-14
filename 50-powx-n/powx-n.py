c2c0c0e3e51fc5c759258b67d2e9efc0d84873bd
Amina Saeed <125123484+aminasaeed223@users.noreply.github.com>
2024-06-14 20:04:18 +0500

    Time: 22 ms (99.29%) | Memory: 16.5 MB (90.37%) - LeetSync

--- 50-powx-n/powx-n.py
+++ 50-powx-n/powx-n.py
@@ -1,7 +1,10 @@
 
 class Solution:
     def myPow(self, x: float, n: int) -> float:
+        # Simplpy returning the power of given no of inputs
         return pow(x,n)
+        y = float(x**n)
+        return y
         
 
         
