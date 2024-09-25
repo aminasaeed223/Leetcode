d087643543245b6077a8f2fc14d2034788001c9a
Amina Saeed <125123484+aminasaeed223@users.noreply.github.com>
2024-09-25 18:12:29 +0500

    Time: 36 ms (59.18%) | Memory: 16.7 MB (17.77%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.py
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.py
@@ -1,14 +1,6 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
-        # using split function to separate each word in string
-        split_words = s.split()
-        # print(split_words)
-        # Now we will reversed these words using slicing
-        reversed_words = split_words[::-1]
-        # print(reversed_words)
-        # we have to return answer in string, so we will convert list into string again separated with spaces
-        reversed_string = " ".join(reversed_words)
-        
-        return reversed_string
-
+       words = s.split()
+       reveresed_words = words[::-1]
+       return " ".join(reveresed_words)
         
